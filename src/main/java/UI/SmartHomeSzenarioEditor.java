5b2b36d91107ee1ccfa38f968384696919bd0882
jonaszb13 <jonas.mueller.jm@gmx.de>
2026-04-09 11:21:00 +0200

    JM GUI
    - Add UiGuiBuilder
    - Add basic UI Structure

---
+++ src/main/java/UI/SmartHomeSzenarioEditor.java
@@ -0,0 +1,11 @@
+package UI;
+import javax.swing.*;
+
+
+public class SmartHomeSzenarioEditor extends JFrame {
+    private JPanel contentPane;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+}
